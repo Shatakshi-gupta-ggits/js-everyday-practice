9861b34761ef352e17363e0946f161ca6138c18a
Shatakshi Gupta <shatakshig2005@gmail.com>
2025-11-15 16:10:40 +0530

    main

---
+++ DOM/addingElem.js
@@ -0,0 +1,8 @@
+// document.createElement(('p'));
+// let box = document.querySelector('.box');
+// box.appendChild(newP);
+
+let btn = document.creatElement('button');
+console.dir('btn');
+btn.innerText = "click me";
+
